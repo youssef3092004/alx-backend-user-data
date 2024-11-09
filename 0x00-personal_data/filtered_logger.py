fec5bb143aedf199d371c174d30748716b5bab67
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-09 18:47:34 +0200

    feat: Implement RedactingFormatter to filter sensitive data in logs
    - Added RedactingFormatter class to format log messages while redacting sensitive fields.
    - Accepts a list of fields to redact and replaces their values with "***" in log messages.
    - Overrides "format" method to apply "filter_datum" to redact specified fields before logging.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,7 +6,7 @@ log message obfuscated
 
 import re
 from typing import List
-
+import logging
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
@@ -15,3 +15,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+class RedactingFormatter(logging.Formatter):
+    """
+        Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """
+            Constructor
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        return super().format(record)

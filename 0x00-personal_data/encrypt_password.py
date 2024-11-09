4b968f17d0541541eff678bfaad3da06849b5121
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-09 21:50:08 +0200

    Implement password validation function using bcrypt
    - Added the is_valid function to validate a password by comparing it with a hashed password.
    - Uses bcrypt.checkpw to securely verify the password.
    - Ensures that the password is properly encoded before comparison.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """Encrypting passwords"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)

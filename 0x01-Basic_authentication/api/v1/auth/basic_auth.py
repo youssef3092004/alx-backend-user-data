432f62e3e044a8d3f0c372b5bf2d85ac022a4af1
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-13 09:11:04 +0200

    fix: make a doc.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,6 +8,7 @@ class BasicAuth(Auth):
 
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
+        """ Basic Authentication Class """
         if authorization_header is None or not isinstance(
                 authorization_header, str):
             return None

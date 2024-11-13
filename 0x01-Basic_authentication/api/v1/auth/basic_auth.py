5e0d477a34b116b151d201f1d3f6d4097ae607fb
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-13 10:13:59 +0200

    feat: Implement user_object_from_credentials method in BasicAuth
    - Added `user_object_from_credentials` method to the `BasicAuth` class.
    - Returns the `User` instance based on the provided email and password.
    - Searches for the user by email and checks if the password is valid using the `is_valid_password` method.
    - Returns `None` if no user is found or the password is invalid.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 """
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -47,3 +49,16 @@ class BasicAuth(Auth):
             if x[i] == ':':
                 return x[:
                          i], x[i + 1:]
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """ Returns the User instance based on his email and password """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        found_users = User.search({'email': user_email})
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None

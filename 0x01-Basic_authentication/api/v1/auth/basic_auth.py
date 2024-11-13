14b60d4e02f5224f086a23d3458c6abd0111b90a
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-13 08:53:34 +0200

    fix: the path of the import the Auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Basic Authentication
 """
-from auth import Auth
-
+from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Basic Authentication Class """

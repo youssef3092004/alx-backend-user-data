841471924ebd77d78ebd455baf41463e1ebb0e73
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 15:16:20 +0200

    Add `user_id_for_session_id` method to `SessionAuth` class
    - Implements retrieval of user ID based on a provided session ID.
    - Returns `None` if session ID is invalid or not found in `user_id_by_session_id`.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -16,3 +16,8 @@ class SessionAuth(Auth):
         session_id = str(uuid4())
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)

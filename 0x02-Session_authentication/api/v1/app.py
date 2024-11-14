3d45d803cea2f4cd15f1ae62b0233fc68272ecab
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 16:48:02 +0200

    test

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,9 +60,9 @@ def before_request() -> None:
         '/api/v1/forbidden/',
         '/api/v1/auth_session/login/']
     if auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request):
+        if auth.authorization_header(request) is None:
             abort(401)
-        if not auth.session_cookie(request):
+        if auth.session_cookie(request) is None:
             abort(401)
         if not auth.current_user(request):
             abort(403)

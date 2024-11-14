ee79819f8927597182fff864dcc6759fcc65b6d6
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 14:43:15 +0200

    Add `SessionAuth` class with `create_session` method for session-based authentication
    - Implemented `SessionAuth` class extending `Auth`.
    - Added `create_session` method that generates a unique session ID for a valid user ID and stores the mapping.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Session Authentication """
+    user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
         """ Create a Session ID """
-        pass
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        SessionAuth.user_id_by_session_id[session_id] = user_id
+        return session_id

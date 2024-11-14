b1d9171cae0a54a18aa20dbd02eacbf9270af9b7
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 16:37:30 +0200

    Add login route for Session Authentication
    - Created `/auth_session/login` endpoint to handle user login.
    - Validates email and password fields; returns appropriate error messages for missing or invalid fields.
    - Searches for user by email, verifies password, and creates a session ID upon successful authentication.
    - Sets session cookie with session ID in the response for authenticated users.

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Module of Session Authentication """
+from api.v1.views import app_views
+from flask import jsonify, request
+from models.user import User
+from os import getenv
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /auth_session/login
+    Return
+        - Logged in user
+    """
+    email = request.form.get('email')
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+    password = request.form.get('password')
+    if password is None:
+        return jsonify({"error": "password missing"}), 400
+    users = User.search({'email': email})
+    if users is None:
+        return jsonify({"error": "no user found for this email"}), 404
+    for user in users:
+        if not user.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user.id)
+    SESSION_NAME = getenv('SESSION_NAME')
+    response = jsonify(user.to_json())
+    response.set_cookie(SESSION_NAME, session_id)
+    return response

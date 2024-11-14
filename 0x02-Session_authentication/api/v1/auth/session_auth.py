09a960016f047161787839bd0ca76a0d4f74aa30
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 16:15:00 +0200

    Implement current_user method to retrieve User instance via session cookie
    - Added `current_user` method to return a User instance based on session cookie value.
    - Retrieves session ID from the request cookie, maps it to user ID, and fetches the User.
    - Returns `None` if session ID or user ID are not valid.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ Module of Session Authentication """
 from api.v1.auth.auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -22,3 +23,13 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Returns a User instance based on a cookie value """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)

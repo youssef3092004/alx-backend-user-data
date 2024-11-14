bdaff8e57cc97f161e9307d4b2b456e991fda9a7
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-14 17:03:23 +0200

    Add logout endpoint for session-based authentication
    - Created `logout` route at `/auth_session/logout` with DELETE method.
    - Calls `destroy_session` to handle session deletion for logged-out users.
    - Returns 404 if session deletion fails, otherwise returns an empty JSON response with status 200.

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Module of Session Authentication """
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify, request, abort
 from models.user import User
 from os import getenv
 
@@ -35,3 +35,19 @@ def login():
     response = jsonify(user.to_json())
     response.set_cookie(SESSION_NAME, session_id)
     return response
+
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """ DELETE /auth_session/logout
+    Return
+        - Empty JSON
+    """
+
+    from api.v1.app import auth
+
+    deleted = auth.destroy_session(request)
+    if deleted is False:
+        abort(404)
+    return jsonify({}), 200

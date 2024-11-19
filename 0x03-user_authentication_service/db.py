504e1cd5a8033eade25929374653540c06d7fe5f
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-19 16:00:29 +0200

    Add method to find a user by keyword arguments
    - Implement find_user_by method in the DB class to query the database
    - Validate that provided kwargs match valid column names in the User table
    - Raise InvalidRequestError if no kwargs are provided or invalid columns are used
    - Return the first user found matching the kwargs, or raise NoResultFound if no user is found

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -4,9 +4,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import InvalidRequestError
 from sqlalchemy.orm.session import Session
 
-from user import Base
+from user import Base   
 
 
 class DB:
@@ -37,5 +39,19 @@ class DB:
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
+        return user
 
+def find_user_by(self, **kwargs) -> User:
+        """ Finds user by key word args
+        Return: First row found in the users table as filtered by kwargs
+        """
+        if not kwargs:
+            raise InvalidRequestError
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
         return user

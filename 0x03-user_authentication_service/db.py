deda3633b6a53d099cc7ed3fff047d32a7519451
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-19 13:48:11 +0200

    Add method to create and save a new user to the database
    - Implement add_user method in the DB class
    - Create a new User instance with email and hashed_password
    - Save the user to the database using the session
    - Commit changes to persist the user in the database
    - Return the newly created User instance

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,3 +29,12 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Save the user to the database
+        """
+        from user import User
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user

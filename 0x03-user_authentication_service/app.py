5d985c6b50b65e42d690883db0c78c32f49d0885
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 01:51:38 +0200

    Add root route to Flask app with a welcome message
    - Implement GET / route to return a welcome message
    - Use jsonify to respond with a JSON object containing the message
    - Add a docstring to explain the route functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -11,6 +11,13 @@ from flask import (Flask,
 app = Flask(__name__)
 AUTH = Auth()
 
+@app.route('/', methods=['GET'], strict_slashes=False)
+def hello() -> str:
+    """ GET /
+    Return:
+      - a welcome message
+    """
+    return jsonify({"message": "Bienvenue"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

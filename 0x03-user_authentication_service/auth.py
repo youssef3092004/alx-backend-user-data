301b74b05d7667597a45cf0c5b1e5223266aeb4b
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-21 17:55:16 +0200

    Add _hash_password function to generate salted password hashes
    - Implement _hash_password to securely hash passwords using bcrypt
    - Encode the input password to UTF-8 for compatibility
    - Return the salted hash as bytes
    - Add a docstring explaining the functions purpose

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
 import bycrypt
-def _hash_password(password):
-    return bycrypt.hashpw(password.encode('uft-8'), bycrypt.gensalt())
+def _hash_password(password) -> bytes:
+    """ Returns a salted hash of the input password """
+    hashed_password = bycrypt.hashpw(password.encode('uft-8'), bycrypt.gensalt())
+    return hashed_password

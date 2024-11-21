76657ffc808395ddc2279dbbdef134cf872e20ad
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 01:58:31 +0200

    Add user registration route to Flask app
    - Implement POST /users route to handle user registration
    - Accept email and password from the request body
    - Use the Auth class to register the user
    - Return a success message with the email if registration is successful
    - Handle duplicate email errors and return an appropriate response

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -21,5 +21,23 @@ def hello() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register_user() -> str:
+    """ POST /users
+    JSON Body:
+      - email
+      - password
+    Return:
+      - user object
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

fab89191ba70da1fe85f5b593d4b482a40572483
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 23:02:20 +0200

    final

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,6 +92,6 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
         except NoResultFound:
             raise ValueError
-        
         hashed_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
+        self._db.update_user(user.id, hashed_password=hashed_password,
+                             reset_token=None)

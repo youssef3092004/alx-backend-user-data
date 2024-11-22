f021cc51948d95c624dfb1042bf33d488ee61128
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 21:33:35 +0200

    Add logout route to handle user session termination
    - Implement DELETE /sessions route to log out users
    - Retrieve session ID from cookies and validate the session
    - Use Auth.get_user_from_session_id to identify the user
    - Invalidate the session by calling Auth.destroy_session
    - Abort with 403 if the session is invalid or user does not exist

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,5 +57,18 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
 
 
+@app.route('/sessions', methods=['DELETE'])
+def logout() -> str:
+    """ DELETE /sessions
+    Return:
+      - redirect to /
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    Auth.destroy_session(user.id)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

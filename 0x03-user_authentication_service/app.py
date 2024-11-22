7ec87fbff900d8db6e73d0006cbdabe97c376311
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 23:30:18 +0200

    Add route for password update with reset token
    - Implement PUT /reset_password route for updating user password
    - Extract email, reset_token, and new_password from request body
    - Call Auth.update_password to reset the users password
    - Return a success message if password update is successful
    - Abort with 403 if a KeyError occurs or the reset token is invalid

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -107,5 +107,27 @@ def get_reset_password_token() -> str:
     return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'])
+def update_password(self, email: str, reset_token: str, new_password: str) -> None:
+    """ PUT /reset_password
+    JSON Body:
+      - email
+      - reset_token
+      - new_password
+    Return:
+      - message
+    """
+    try:
+      email = request.form.get('email')
+      reset_token = request.form.get('reset_token')
+      new_password = request.form.get('new_password')
+    except KeyError:
+        abort(403)
+    try:
+      Auth.update_password(reset_token, new_password)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")

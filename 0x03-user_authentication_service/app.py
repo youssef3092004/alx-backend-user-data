9e4c7a6f9fe3e60175c07d2ab3c0e34290ac3872
youssef <youssefahmedabdelkader25@gmail.com>
2024-11-22 21:36:14 +0200

    final

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -64,6 +64,8 @@ def logout() -> str:
       - redirect to /
     """
     session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
         abort(403)
